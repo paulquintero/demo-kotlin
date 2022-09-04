5f1fbe6f5e5aa63fcd8d9309b67e654384fe351c
Paul Quintero <paul.alonzoqr@gmail.com>
2022-09-03 22:34:20 -0600

    fix index

--- src/main/resources/db/migration/V1.1__Insert_Records_CatPersonas.sql
+++ src/main/resources/db/migration/V1.1__Insert_Records_CatPersonas.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.cat_personas(
 	id, nombre, ine, appaterno, apmaterno, fechanacimiento)
-	VALUES (2, 'JOJOPEPE19861121','Jose', 'Jose', 'Pepe', '1986-11-21');
+	VALUES (1, 'JOJOPEPE19861121','Jose', 'Jose', 'Pepe', '1986-11-21');
